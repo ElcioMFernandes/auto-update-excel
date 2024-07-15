02fc2ceff38f48f49055c6d2941389d011593425
Elcio Mateus Fernandes <elciomateusfernandes1@gmail.com>
2024-07-15 13:20:26 -0300

    chore: Add files to .gitignore and update spreadsheet.json

--- main.py
+++ main.py
@@ -1,85 +1,121 @@
 import win32com.client as win32
-import os, log, json, sys
-log = log.Logger()
+import os, json, logging, sys, datetime
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    filename=f"{datetime.datetime.today().strftime('%d%m%Y')}.log",
+                    filemode='a')
 class AutoUpdate:
+    __excelApp = None
+    __JsonFile = None
     __jsonData = None
-    __excel = None
+    
+    def setJsonFile(self, jsonFile) -> None:
+        self.__JsonFile = jsonFile
+
+    def getJsonFile(self) -> str:
+        return self.__JsonFile
+    
+    def setJsonData(self, jsonData) -> None:
+        self.__jsonData = jsonData
+
+    def getJsonData(self) -> dict:
+        return self.__jsonData
+
+    def setExcelApp(self) -> None:
+        self.__excelApp = win32.gencache.EnsureDispatch('Excel.Application')
+        self.__excelApp.Visible = True
+
+    def getExcelApp(self) -> object:
+        return self.__excelApp
 
     def __init__(self) -> None:
+        logging.info("Construtor inicializado.")
+        
         try:
-            log.w("Inicializando objeto.")
-            self.setJsonData(self.consumeJson())
-            self.__excel = win32.gencache.EnsureDispatch('Excel.Application') 
-        except Exception as e:
-            log.w("ERROR", str(e))
+            self.setExcelApp()
+            logging.info("ExcelApp inicializado.")
+        
+            if os.path.isfile(os.path.join(os.getcwd(),'spreadsheets.json')):
+        
+                try:
+                    self.setJsonFile('spreadsheets.json')
+                    logging.info("Arquivo Json setado.")
+        
+                    try:
+        
+                        with open(self.getJsonFile(), 'r', encoding='utf-8') as jsonFileRead:
+                            self.setJsonData(json.load(jsonFileRead))                        
+                            logging.info("Arquivo Json lido.")
+        
+                    except Exception as e:
+                        logging.error(f"Falha ao ler arquivo de configuração: {e}")
+                        self.getExcelApp().Quit()
+                        sys.exit(1)
 
-    def setJsonData(self, content) -> None:
-        try:
-            log.w("Conteúdo do Json sendo alocado na memória.")
-            self.__jsonData = content
-        except Exception as e:
-            log.w("ERROR", str(e))
+                except Exception as e:
+                    logging.error(f"Falha ao setar o arquivo de configuração: {e}")
+                    self.getExcelApp().Quit()
+                    sys.exit(1)
 
-    def getJsonData(self):
-        try:
-            log.w("Consumindo conteúdo do Json alocado em memória.")
-            return self.__jsonData
         except Exception as e:
-            log.w("ERROR", str(e))
-
-    def consumeJson(self):
-        try:
-            log.w("Iniciando leitura do Json.")
-            with open('spreadsheets.json', 'r', encoding='utf-8') as jsonFile:
-                return json.load(jsonFile)
-        except Exception as e:
-            log.w("ERROR", str(e))
+            logging.error(f"Falha ao iniciar aplicativo: {e}")
+            sys.exit(1)
 
     def main(self) -> None:
-        try:
-            for dirs, info in self.getJsonData()['dirs'].items():
-                log.w(f"Iterando sobre o diretório: {info['spreadsheet_route']}.")
-                for file in info["spreadsheets_files"]:
-                    self.update(os.path.join(info['spreadsheet_route'], file))
+        for directory, information in self.getJsonData().items():
+            logging.info(f"Chave: {directory}.")
+
+            if os.path.isdir(information['route']):
+                logging.info(f"Rota de {directory} existente.")
+
+                for file in information['files']:
+                    logging.info(f"Arquivo: {file}")
+
+                    if os.path.isfile(os.path.join(information['route'], f"{file}.{information['type']}")):
+                        logging.info(f"Arquivo: {file} existente.")
+                        self.update(os.path.join(information['route'], f"{file}.{information['type']}"))
+                    
+                    else:
+                        logging.error(f"O arquivo {file}, indicado em {directory} não existe")
+                        pass
+
             else:
-                log.w('        Fechando Excel.')
-                self.__excel.Quit() # Fechar o Excel
-                log.w("Fim da aplicação")
-                sys.exit(0)
+                logging.error(f"A rota indicada em {directory} não existe")
+                pass
 
-        except Exception as e:
-            log.w("ERROR", f"{e}")
+        else:
+            logging.info("Fechando ExcelApp e encerrando aplicação.")
+            self.getExcelApp().Quit()
+            sys.exit(0)
 
-    def update(self, file) -> None:
-        log.w(f"    Atuando sobre o arquivo {file}")
+    def update(self, fileToUpdate) -> None:
+        logging.info(f"Iniciando atualização do arquivo {fileToUpdate}")
+        
         try:
-
-            log.w('        Executando Excel em background.')
-            self.__excel.Visible = True # Executar em background
-            #excel.Visible = False # Executar em background
-
-            log.w('        Abrindo arquivo em workbook.')
-            #workbook = excel.Workbooks.Open(file) # Abrir o arquivo
-            workbook = self.__excel.Workbooks.Open(os.path.join(os.getcwd(), file)) # Abrir o arquivo
-
-            log.w('        Atualizando conexões.')
-            self.__excel.ActiveWorkbook.RefreshAll() # Atualizar todas as conexões
-
-            log.w('        Salvando workbook.')
-            workbook.Save() # Salvar
-
-            log.w('        Fechando workbook')
-            workbook.Close() # Fechar
-
+            workbook = self.getExcelApp().Workbooks.Open(fileToUpdate)
+            logging.info("Workbook iniciado.")
+        
+            try:
+                self.getExcelApp().ActiveWorkbook.RefreshAll()
+                logging.info("Arquivo atualizado.")
+        
+                try:
+                    workbook.Save()
+                    logging.info("Arquivo salvo.")
+                    workbook.Close()
+                    logging.info("Workbook fechado.")
+        
+                except Exception as e:
+                    logging.error(f"Erro ao salvar ou fechar: {e}")
+        
+            except Exception as e:
+                logging.error(f"Erro ao atualizar tudo: {e}")
+        
         except Exception as e:
-            log.w("ERROR", f"        {e}")
+            logging.error(f"Erro ao abrir o arquivo: {e}")
 
-if __name__ == "__main__":
-    try:
-        log.w("Início da aplicação.")
-        app = AutoUpdate()
-        app.main()
-
-    except Exception as e:
-        log.w("ERROR", str(e))
+if __name__ == '__main__':
+    app = AutoUpdate()
+    app.main()
+    
