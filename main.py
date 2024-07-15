a003f19fb8b20f5c810083e4fa9fc37bc463e03d
Elcio Mateus Fernandes <elciomateusfernandes1@gmail.com>
2024-07-15 14:51:55 -0300

    update main

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import win32com.client as win32
 import os, json, logging, sys, datetime
+from tkinter import messagebox
+
+if os.path.isdir(os.path.join(os.getcwd(),'log')) == False:
+    os.makedirs('log')
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s',
-                    filename=f"{datetime.datetime.today().strftime('%d%m%Y')}.log",
-                    filemode='a')
+                    filename=f"log\{datetime.datetime.now().strftime('%d-%m-%Y-%H-%M-%S')}.log",
+                    filemode='a',
+                    encoding='utf-8')
+
 class AutoUpdate:
     __excelApp = None
     __JsonFile = None
@@ -24,7 +30,7 @@ class AutoUpdate:
 
     def setExcelApp(self) -> None:
         self.__excelApp = win32.gencache.EnsureDispatch('Excel.Application')
-        self.__excelApp.Visible = True
+        self.__excelApp.Visible = False
 
     def getExcelApp(self) -> object:
         return self.__excelApp
@@ -63,31 +69,44 @@ class AutoUpdate:
             sys.exit(1)
 
     def main(self) -> None:
-        for directory, information in self.getJsonData().items():
-            logging.info(f"Chave: {directory}.")
+        try: 
+            for directory, information in self.getJsonData().items():
+                logging.info(f"Chave: {directory}.")
 
-            if os.path.isdir(information['route']):
-                logging.info(f"Rota de {directory} existente.")
+                if os.path.isdir(information['route']):
+                    logging.info(f"Rota de {directory} existente.")
 
-                for file in information['files']:
-                    logging.info(f"Arquivo: {file}")
+                    for file in information['files']:
+                        logging.info(f"Arquivo: {file}")
 
-                    if os.path.isfile(os.path.join(information['route'], f"{file}.{information['type']}")):
-                        logging.info(f"Arquivo: {file} existente.")
-                        self.update(os.path.join(information['route'], f"{file}.{information['type']}"))
-                    
-                    else:
-                        logging.error(f"O arquivo {file}, indicado em {directory} não existe")
-                        pass
+                        if os.path.isfile(os.path.join(information['route'], f"{file}.{information['type']}")):
+                            logging.info(f"Arquivo: {file} existente.")
+                            self.update(os.path.join(information['route'], f"{file}.{information['type']}"))
+                        
+                        else:
+                            logging.error(f"O arquivo {file}, indicado em {directory} não existe")
+                            pass
+
+                else:
+                    logging.error(f"A rota indicada em {directory} não existe")
+                    pass
 
             else:
-                logging.error(f"A rota indicada em {directory} não existe")
-                pass
+                logging.info("Fechando ExcelApp e encerrando aplicação.")
+                self.getExcelApp().Quit()
+                sys.exit(0)
 
-        else:
-            logging.info("Fechando ExcelApp e encerrando aplicação.")
+        except AttributeError:
             self.getExcelApp().Quit()
-            sys.exit(0)
+            self.createJson()
+            messagebox.showerror(f"Erro","Arquivo Json vazio ou inexistente, preencher corretamente.\nEm caso de dúvida no preenchimento contatar Élcio TIC.")
+            logging.error("Fim da aplicação por conta de json mal configurado ou inexistente.")
+            sys.exit(1)
+
+        except Exception as e:
+            self.getExcelApp().Quit()
+            logging.error(e)
+            sys.exit(1)
 
     def update(self, fileToUpdate) -> None:
         logging.info(f"Iniciando atualização do arquivo {fileToUpdate}")
@@ -114,6 +133,31 @@ class AutoUpdate:
         
         except Exception as e:
             logging.error(f"Erro ao abrir o arquivo: {e}")
+
+    def createJson(self):
+        data = {
+            "nome_identificacao_chave": {
+                "owner": "proprietario_pasta",
+                "route": "caminho_pasta",
+                "type": "xlsx",
+                "files": [
+                    "nome_arquivo_sem_extensao1",
+                    "nome_arquivo_sem_extensao2"
+                ]
+            },
+            "nome_de_identificacao_chave": {
+                "owner": "proprietario_pasta",
+                "route": "caminho_pasta",
+                "type": "xlsx",
+                "files": [
+                    "nome_arquivo_sem_extensao3",
+                    "nome_arquivo_sem_extensao4"
+                ]
+            }
+        }
+
+        with open('spreadsheets.json', 'w') as json_file:
+            json.dump(data, json_file, indent=4)
 
 if __name__ == '__main__':
     app = AutoUpdate()
