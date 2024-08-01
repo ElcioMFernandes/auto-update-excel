4104151cf551aa82806d5a893232759ade6f48f4
Elcio Mateus Fernandes <elciomateusfernandes1@gmail.com>
2024-08-01 15:35:53 -0300

    Adicionado bloco try para importação do módulo win32, Adicionado novas informações no log e novas opções configuraveis dentro do json, como visibilidade do excel, e tempo entre atualização das planilhas.

--- main.py
+++ main.py
@@ -1,92 +1,115 @@
-import win32com.client as win32
-import os, json, logging, sys, datetime
-from tkinter import messagebox
+import os, sys, json, time, logging, datetime, tkinter.messagebox
+
+try:
+    import win32com.client as win32
+except Exception as e:
+    tkinter.messagebox.showerror('Erro','Erro ao importar a biblioteca win32com.client')
+    sys.exit(1)
+
 #import schedule
 
 class AutoUpdate:
     __excelApp = None
     __jsonFile = None
     __jsonData = None
+    __interval = None
+    __visible  = None
     
-    def setJsonFile(self, jsonFile:str) -> None:
-        """
-        Set a value to the private jsonFile attribute.
-        """
+    def __setVisible(self, visibility: bool) -> None:
+        """ Método que atribui um valor passado por parâmetro para o atributo privado __visible. """
+        self.__visible = visibility
+
+    def __getVisible(self) -> bool:
+        """ Método para pegar o valor do atributo privado __visible. """
+        return self.__visible
+
+    def __setInterval(self, time: int) -> None:
+        """ Método para setar o tempo de intervalo entre a execução das atualizações de planilha. """
+        self.__interval = time
+
+    def __getInterval(self) -> int:
+        """ Método para pegar o valor do atributo privado __interval, responsável pelo tempo de intervalo entre a execução das atualizações de planilha. """
+        return self.__interval
+
+    def __setJsonFile(self, jsonFile:str) -> None:
+        """ Set a value to the private jsonFile attribute. """
         self.__jsonFile = jsonFile
 
-    def getJsonFile(self) -> str:
-        """
-        Get on jsonFile private attribute value.
-        """
+    def __getJsonFile(self) -> str:
+        """ Get on jsonFile private attribute value. """
         return self.__jsonFile
     
-    def setJsonData(self, jsonData:dict) -> None:
-        """
-        Set a value to the private jsonData attribute.
-        """
+    def __setJsonData(self, jsonData:dict) -> None:
+        """ Set a value to the private jsonData attribute. """
         self.__jsonData = jsonData
 
-    def getJsonData(self) -> dict:
-        """
-        Get on jsonData private attribute value.
-        """
+    def __getJsonData(self) -> dict:
+        """ Get on jsonData private attribute value. """
         return self.__jsonData
 
-    def setExcelApp(self) -> None:
-        """
-        Set in the excelApp private attribute that stores the win32com library object.
-        """
+    def __setExcelApp(self) -> None:
+        """ Set in the excelApp private attribute that stores the win32com library object. """
         self.__excelApp = win32.gencache.EnsureDispatch('Excel.Application')
-        self.__excelApp.Visible = False
+        self.__excelApp.Visible = self.__getVisible()
 
-    def getExcelApp(self) -> object:
-        """
-        Returns the ExcelApp object from the win32com library.
-        """
+    def __getExcelApp(self) -> object:
+        """ Returns the ExcelApp object from the win32com library. """
         return self.__excelApp
 
     def __init__(self) -> None:
-        """
-        Class constructor.
-        """
-        logging.info("Iniciando construtor")
-        try:
-            self.setExcelApp()
-            if os.path.isfile(os.path.join('C:\\Users', os.getlogin(), 'auto update','spreadsheets.json')):
-                try:
-                    self.setJsonFile('spreadsheets.json')
-                    try:        
-                        with open(self.getJsonFile(), 'r', encoding='utf-8') as jsonFileRead:
-                            self.setJsonData(json.load(jsonFileRead))
-                            logging.info("Construtor finalizado.")                      
-                    except Exception as e:
-                        logging.error(f"Falha ao ler arquivo de configuração: {e}")
-                        self.getExcelApp().Quit()
-                        sys.exit(1)
+        """ Classe construtora. """
+        if os.path.isfile(os.path.join('C:\\Users', os.getlogin(), 'auto update','spreadsheets.json')): # Se o arquivo existir
 
-                except Exception as e:
-                    logging.error(f"Falha ao setar o arquivo de configuração: {e}")
-                    self.getExcelApp().Quit()
-                    sys.exit(1)
-            else:
-                self.getExcelApp().Quit()
-                self.createJson()
-                messagebox.showerror(f"Erro no init","Arquivo Json vazio ou inexistente.")
-                logging.error("Arquivo Json vazio ou inexistente.")
+            self.__setJsonFile(os.path.join('C:\\Users', os.getlogin(), 'auto update','spreadsheets.json')) # Atribui o __jsonFile como 'spreadsheets.json'
+            logging.info('Arquivo Json setado.')
+
+            try:        
+                with open(self.__getJsonFile(), 'r', encoding='utf-8') as jsonFileRead:
+                        try:
+                            self.__setJsonData(json.load(jsonFileRead)) # Atribiu o conteúdo do Json ao __jsonData
+                            logging.info('Conteúdo do arquivo json carregado em variável.')
+
+                        except Exception as e:
+                            logging.error('Conteúdo do json corrompido ou vazio.')
+                        
+                        try:
+                            self.__setVisible(self.__getJsonData()['settings']['visible']) # Atribui o valor do dicionário settings, chave visible, a variável __visible
+                            logging.info(f'Visibilidade da execução definida como {self.__getVisible()}.')
+
+                        except Exception as e:
+                            logging.error(logging.error('Conteúdo do json corrompido ou vazio no trecho settings, visible.'))
+
+                        try:
+                            self.__setInterval(self.__getJsonData()['settings']['interval']) # Atribui o valor do dicionário settings, chave interval, a variável __interval
+                            logging.info(f'Intevalo entre a atualização definido para {self.__getInterval()} segundos.')
+                        except Exception as e:
+                            logging.error(logging.error('Conteúdo do json corrompido ou vazio no trecho settings, interval.'))
+
+                        try:
+                            self.__setExcelApp() # Chama o método ExcelApp
+                            logging.info('Aplicativo Excel inicializado.')
+                            
+                        except Exception as e:
+                            logging.critical(f"Falha no método __init__: {e}")
+                            self.__getExcelApp().Quit()
+                            sys.exit(1)
+
+            except Exception as e:
+                logging.critical(f"Falha no método __init__: {e}")
                 sys.exit(1)
 
-        except Exception as e:
-            logging.error(f"Falha ao iniciar aplicativo: {e}")
+        else:
+            self.__createJson()
+            logging.error("Arquivo Json vazio ou inexistente.")
             sys.exit(1)
 
     def main(self) -> None:
-        """
-        Main application method.
-        """
-        logging.info("Iniciando bloco principal.\n")
+        """ Main application method. """
+        logging.info("Iniciando método main.\n")
+
         try: 
-            for directory, information in self.getJsonData().items():
-                if os.path.isdir(information['route']):
+            for directory, information in self.__getJsonData().items():
+                if directory != 'settings' and os.path.isdir(information['route']):
                     logging.info(f"+{directory.replace('_', ' ').capitalize()}")
                     logging.info(f"|__{information['route']}")                    
                 
@@ -95,42 +118,42 @@ class AutoUpdate:
                         logging.info(f"   |__Arquivo: {file}")
                 
                         if os.path.isfile(os.path.join(information['route'], f"{file}.{information['type']}")):
-                            self.update(os.path.join(information['route'], f"{file}.{information['type']}"), is_last_file)
+                            self.__update(os.path.join(information['route'], f"{file}.{information['type']}"), is_last_file)
+                            
+                            time.sleep(self.__getInterval())
                 
                         else:
                             logging.error(f"O arquivo {file}, indicado em {directory} não existe")
                             pass
 
                 else:
-                    logging.error(f"A rota indicada em {directory} não existe")
+                    if directory != 'settings':
+                        logging.error(f"A rota indicada em {directory} não existe")
                     pass
 
             else:
-                self.getExcelApp().Quit()
+                self.__getExcelApp().Quit()
                 logging.info("Finalizado todos os arquivos.")
                 sys.exit(0)
 
         except AttributeError:
-            self.getExcelApp().Quit()
-            self.createJson()
-            messagebox.showerror(f"Erro na main","Arquivo Json vazio ou inexistente.")
+            self.__getExcelApp().Quit()
+            self.__createJson()
             logging.error("Arquivo Json vazio ou inexistente.")
             sys.exit(1)
 
         except Exception as e:
-            self.getExcelApp().Quit()
+            self.__getExcelApp().Quit()
             logging.error(e)
             sys.exit(1)
 
-    def update(self, fileToUpdate:str, last_file) -> None:
-        """
-        Method that updates data in the spreadsheet.
-        """
+    def __update(self, fileToUpdate:str, last_file) -> None:
+        """ Method that updates data in the spreadsheet. """
         try:
-            workbook = self.getExcelApp().Workbooks.Open(fileToUpdate)
+            workbook = self.__getExcelApp().Workbooks.Open(fileToUpdate)
         
             try:
-                self.getExcelApp().ActiveWorkbook.RefreshAll()
+                self.__getExcelApp().ActiveWorkbook.RefreshAll()
                 try:
                     workbook.Save()
                     workbook.Close()
@@ -148,23 +171,25 @@ class AutoUpdate:
         except Exception as e:
             logging.error(f"Erro ao abrir o arquivo: {e}")
 
-    def createJson(self):
-        """
-        Method for creating a json template if it does not exist.
-        """
+    def __createJson(self):
+        """ Method for creating a json template if it does not exist. """
         data = {
-            "nome_identificacao_chave": {
-                "owner": "proprietario_pasta",
-                "route": "caminho_pasta",
+            "settings":{
+                "visible": True,
+                "interval": 10
+            },
+            "nome_de_identificacao_chave1": {
+                "owner": "proprietario_pasta1",
+                "route": "caminho_pasta1",
                 "type": "xlsx",
                 "files": [
                     "nome_arquivo_sem_extensao1",
                     "nome_arquivo_sem_extensao2"
                 ]
             },
-            "nome_de_identificacao_chave": {
-                "owner": "proprietario_pasta",
-                "route": "caminho_pasta",
+            "nome_de_identificacao_chave2": {
+                "owner": "proprietario_pasta2",
+                "route": "caminho_pasta2",
                 "type": "xlsx",
                 "files": [
                     "nome_arquivo_sem_extensao3",
@@ -173,7 +198,7 @@ class AutoUpdate:
             }
         }
 
-        with open('spreadsheets.json', 'w') as json_file:
+        with open(os.path.join('C:\\Users', os.getlogin(), 'auto update','spreadsheets.json'), 'w') as json_file:
             json.dump(data, json_file, indent=4)
 
 if __name__ == '__main__':
@@ -188,8 +213,7 @@ if __name__ == '__main__':
 
     try:
         app = AutoUpdate()
-        logging.info("Instância do objeto AutoUpdate inicializada.")
         app.main()
+
     except Exception as e:
         logging.error(e)
-    
